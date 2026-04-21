2f8f33fb42cbe891ca6700ba8b3f92cc86b8ffb9
vanshgolia <vanshror77@gmail.com>
2026-04-21 22:05:24 +0530

    Create User.js

---
+++ exp-2.2.3-transactions/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true, minlength: 6 },
+    accountNumber: { type: String, unique: true },
+    balance: { type: Number, default: 0, min: 0 }
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function () {
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  if (!this.accountNumber) {
+    this.accountNumber = 'ACC' + Date.now() + Math.floor(Math.random() * 1000);
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
